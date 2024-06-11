f84c339c42b24c3bbde51536a34c27f4d6dc8d17
Sergei Avdoshkin <iocaster@Sergeis-MacBook-Air.local>
2024-06-11 09:15:53 +0800

    NuxtLink

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Products details for {{ id }}</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur necessitatibus vitae magni ipsam illo tenetur omnis sed distinctio delectus sequi.</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params;
+</script>
+
+<style lang="scss" scoped>
+
+</style>
